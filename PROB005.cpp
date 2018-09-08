05a74e6a24d48e6760f14d794711cd34d93c3fa1
Sabuj Jana <sabujlovescoding@gmail.com>
2018-09-08 19:12:54 +0530

    adding 2 more solutions to prob005

--- PROB005.cpp
+++ PROB005.cpp
@@ -55,3 +55,94 @@ int main() {
      
     return 0;
 }
+
+
+//  ALTERNATIVE SOLN WHERE BOOST FUNCTION IS INCLUDED
+
+// #include <iostream>
+// #include<boost/math/common_factor.hpp>
+// using namespace std;
+
+// int main() {
+//     //cout<<"GfG!";
+    
+//     int t;
+//     cin>>t;
+    
+//     while(t--)
+//     {
+//         int n;
+//         cin>>n;
+        
+//         int lcm=1;
+//         for (int i = 2; i <= n; i++) {
+//             /* code */
+//             lcm= boost::math::lcm(i,lcm);
+            
+//         }
+        
+        
+//         cout<<lcm<<"\n";
+        
+        
+        
+        
+//     }
+    
+    
+    
+//     return 0;
+// }
+
+
+
+// 2......#include <bits/stdc++.h>
+//  
+// using namespace std;
+
+
+// int findlcm(int a, int b)
+// {
+//     return (a*b)/__gcd(a,b);
+    
+    
+// }
+
+
+
+// 2.  int main() {
+//     //cout<<"GfG!";
+    
+//     int t;
+//     cin>>t;
+    
+//     while(t--)
+//     {
+//         int n;
+//         cin>>n;
+        
+//         int lcm=1;
+//         for (int i = 2; i <= n; i++) {
+//              code 
+             
+//             lcm= findlcm(i,lcm);
+            
+//         }
+        
+//         cout<<lcm<<"\n";
+         
+        
+        
+        
+        
+//     }
+    
+    
+    
+//     return 0;
+// }
+
+
+
+
+
